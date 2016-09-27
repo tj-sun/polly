9e06a3d7ba3e57cfb31bfcbe9e9146eef2d4f2a5
tj-sun <tianjiao.sun.2010@gmail.com>
2016-09-27 17:56:44 +0100

    remove temp file

--- test/TJ/PolyBench/1DVECDOUBLE/run.sh
+++ test/TJ/PolyBench/1DVECDOUBLE/run.sh
@@ -3,4 +3,4 @@
 /home/tianjiao/projects/3.9/ComputeCpp-CE-0.1-Linux/bin/compute++ -cc1 -emit-llvm-bc -triple spir64-unknown-unknown -o VecDouble.spir VecDouble.cl -std=CL1.2
 llvm-dis VecDouble.spir
 opt -S -polly-canonicalize VecDouble.spir.ll > VecDouble.preopt.ll
-# opt -polly-scops -analyze test0.preopt.ll -polly-process-unprofitable -polly-opencl-kernel -debug
+opt -polly-scops -analyze VecDouble.preopt.ll -polly-process-unprofitable -polly-opencl-kernel -debug
